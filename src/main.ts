30c4662498ae3950a4610bab7d2b11b6406ddc8e
Kiang Kuang <kiangkuang@gmail.com>
2022-02-28 23:35:22 +0800

    use vite

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
