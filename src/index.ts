38003315264e5caaaf4bad730199ec78f64a2dfb
Kiang Kuang <kiangkuang@gmail.com>
2022-02-28 19:23:53 +0800

    add light

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 30;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -10,9 +10,19 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 window.document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.OctahedronGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ccff, wireframe: true });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ccff });
 const octahedron = new THREE.Mesh(geometry, material);
 scene.add(octahedron);
+
+const light = new THREE.PointLight(0xffffff);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(light);
+scene.add(lightHelper);
 
 function animate() {
     requestAnimationFrame(animate);
